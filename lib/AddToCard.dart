d6e632695dfb4cb6fd9a2cdde5846d5b697ec4cc
Asmit11 <sandeepgahatraj9@gmail.com>
2023-07-06 07:31:06 +0545

    added class extend to add to cart

--- lib/AddToCard.dart
+++ lib/AddToCard.dart
@@ -1,131 +1,93 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
-class MenuItem {
-  final String id;
+class Product {
   final String name;
   final double price;
 
-  MenuItem(this.id, this.name, this.price);
+  Product({required this.name, required this.price});
 }
 
-class ShoppingCartItem {
-  final MenuItem menuItem;
+class CartItem {
+  final Product product;
   int quantity;
 
-  ShoppingCartItem(this.menuItem, this.quantity);
+  CartItem({required this.product, this.quantity = 1});
 }
 
-class ShoppingCart {
-  List<ShoppingCartItem> items = [];
+class Cart {
+  final List<CartItem> items = [];
 
-  void addItem(MenuItem menuItem, int quantity) {
-    var existingItemIndex =
-    items.indexWhere((item) => item.menuItem.id == menuItem.id);
+  void addToCart(Product product) {
+    bool itemExists = false;
 
-    if (existingItemIndex != -1) {
-      items[existingItemIndex].quantity += quantity;
-    } else {
-      items.add(ShoppingCartItem(menuItem, quantity));
-    }
-  }
-
-  void removeItem(MenuItem menuItem) {
-    items.removeWhere((item) => item.menuItem.id == menuItem.id);
-  }
-
-  void clearCart() {
-    items.clear();
-  }
-
-  double calculateTotalPrice() {
-    double totalPrice = 0;
-
-    for (var item in items) {
-      totalPrice += item.menuItem.price * item.quantity;
+    for (CartItem item in items) {
+      if (item.product.name == product.name) {
+        item.quantity++;
+        itemExists = true;
+        break;
+      }
     }
 
-    return totalPrice;
-  }
-
-  void printCart() {
-    print('┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓');
-    print('┃           Shopping Cart           ┃');
-    print('┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫');
-    for (var item in items) {
-      print(
-          '┃ ${item.menuItem.name} - Quantity: ${item.quantity.toString().padLeft(2)} ┃');
+    if (!itemExists) {
+      items.add(CartItem(product: product));
     }
-    print('┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫');
-    print('┃ Total Price: \$${calculateTotalPrice().toStringAsFixed(2).padLeft(7)} ┃');
-    print('┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛');
   }
 }
 
-class AddToCart extends StatefulWidget {
-  const AddToCart({Key? key}) : super(key: key);
-
-  @override
-  State<AddToCart> createState() => _AddToCartState();
-}
-
-class _AddToCartState extends State<AddToCart> {
-  @override
-  void initState() {
-    super.initState();
-    // Initialize Firebase
-    FirebaseFirestore firestore = FirebaseFirestore.instance;
-
-    // Sample menu items
-    MenuItem item1 = MenuItem('1', 'Cheeseburger', 10.99);
-    MenuItem item2 = MenuItem('2', 'French Fries', 8.99);
-    MenuItem item3 = MenuItem('3', 'Soft Drink', 12.99);
-
-    // Create a new shopping cart
-    ShoppingCart cart = ShoppingCart();
-
-    // Add items to the shopping cart
-    cart.addItem(item1, 2);
-    cart.addItem(item2, 1);
-    cart.addItem(item3, 3);
-
-    // Print the shopping cart
-    cart.printCart();
-
-    // Save the shopping cart to Firestore
-    saveCartToFirestore(firestore, cart);
-  }
-
-  void saveCartToFirestore(
-      FirebaseFirestore firestore, ShoppingCart cart) async {
-    // Create a new Firestore collection reference for shopping carts
-    CollectionReference cartRef = firestore.collection('carts');
-
-    // Create a new Firestore document reference for the current cart
-    DocumentReference docRef = await cartRef.add({});
-
-    // Save each cart item as a subcollection in Firestore
-    for (var item in cart.items) {
-      await docRef.collection('items').add({
-        'menuItemId': item.menuItem.id,
-        'quantity': item.quantity,
-      });
-    }
-
-    print('Shopping cart saved to Firestore with ID: ${docRef.id}');
-  }
+class AddToCart extends StatelessWidget {
+  final Cart cart = Cart();
+  final List<Product> products = [
+    Product(name: 'Product 1', price: 10.99),
+    Product(name: 'Product 2', price: 19.99),
+    Product(name: 'Product 3', price: 7.99),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Container(); // Placeholder widget for the stateful widget
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Product List'),
+      ),
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          Product product = products[index];
+          return ListTile(
+            title: Text(product.name),
+            subtitle: Text('\$${product.price}'),
+            trailing: IconButton(
+              icon: Icon(Icons.add_shopping_cart),
+              onPressed: () {
+                cart.addToCart(product);
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text('Success'),
+                      content: Text('Product added to cart!'),
+                      actions: [
+                        TextButton(
+                          child: Text('OK'),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+            ),
+          );
+        },
+      ),
+    );
   }
 }
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(
-    MaterialApp(
-      home: AddToCart(),
-    ),
-  );
+  runApp(MaterialApp(
+    title: 'Add to Cart',
+    home: ProductListScreen(),
+  ));
 }
